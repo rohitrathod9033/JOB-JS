7ac1d879f404b439bb4a039d1ba8398c13727c68
Rohit Rathod <rohitrathod60371@gmail.com>
2025-09-09 11:26:17 +0530

    Push

---
+++ sciprt.js
@@ -0,0 +1,35 @@
+/*    VARIABLE
+let a = 12;
+let b  = 12;
+console.log(a + b)
+
+    TYPE OF
+let a = false;
+console.log(typeof(a));
+
+    NAN
+let a = 0/0;
+console.log(a);
+
+         OPRATOR : 
+UNICODE --> har number ko ak number assign hota haiii A.B,C == 65,66,67
+'a'>'A' = true
+let a = 'a';
+let b  = 'A';
+
+console.log(a>b);
+console.log(a<b);
+
+     OPRATOR PRECEDENCE
+(),  **,  * / %,  + -
+
+*/
+
+// let Student = {
+//     name1 : "Rohit",
+//     name2 : "Rathod",
+//     age : 22,
+//     course : "MCA"
+// }
+// let fullName = Student.name1 +" " + Student.name2;
+// console.log(fullName);
